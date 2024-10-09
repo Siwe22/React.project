f360f205f052c65209fb9dc6492c67d1ec2e627a
Siwe22 <122867460+Siwe22@users.noreply.github.com>
2024-10-09 11:06:22 +0200

    added a header.js file

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+function Header () {
+
+    return(
+    <header>
+    <h4 style={{color: "blueviolet"}}>
+        <p>Hello, there welcome to my expense tracker, this app allows me to identify and manage my spending in an efficient time frame. 
+            It allows me to plan accordingly for the month.
+        
+        </p>
+        </h4> 
+    </header>
+    );
+    }
+    export default Header 
