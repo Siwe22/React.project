5e30b596997f0142f4282ba6ce867bad3e437cd8
Siwe22 <122867460+Siwe22@users.noreply.github.com>
2024-10-09 10:50:23 +0200

    added information on app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/home' ;
+import Header from './components/header'
+import Table from './components/table'
+import List from './components/list'
+import image0001 from './assets/image00001.jpeg'
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+     
+     <div className="App">
+      <img src={require('./assets/image00001.jpeg')} height ={250} width={200} />
+      <Home />
+      <Header/>
+      <List/>
+  
+      
+      
+
+      </div>
+
+);
 }
 
 export default App;
+
